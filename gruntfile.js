f4619b5571d830ebac51c97dd6191710bb48a83e
db-developer <db-developer@gmx.de>
2019-04-27 23:37:09 +0200

    initial commit Signed-off-by: db-developer <db-developer@gmx.de>

---
+++ gruntfile.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const path    = require( "path" );
+
+const ANGULAR = "angular";
+const BUILD   = "build";
+const CONFIG  = "config";
+const DIST    = "dist";
+const LIB     = "lib";
+
+module.exports = function( grunt ) {
+  // set grunt options
+  grunt.option( "pkgjson",  grunt.file.readJSON( "package.json"  ));
+  grunt.option( "versions", grunt.file.readJSON( "versions.json" ));
+
+  grunt.initConfig({
+
+    angularjson: {
+      template:     "config/angular/angular.json",  // file relative to gruntfile.js
+      build:        BUILD,                          // build directory - relative to gruntfile.js
+      fragments:    [                               // search fragments in library directories
+                      "**/angular.lib.json"         // relative to library directory
+                    ],
+      libs:         [ "lib-*" ],                    // directory pattern(s) for libraries
+    },
+
+    clean: {
+      build:        [ `${BUILD}/**/*` ],
+      dist:         [ `${DIST}/**/*`  ]
+    },
+
+    cleanempty: {
+      // remove empty folders in build and dist directories
+      always: {
+        src:        [ `${BUILD}/**/*`, `${DIST}/**/*` ]
+      },
+    },
+
+    copy: {
+      prerequisites_angular: {
+        files: [{
+          expand:   true,
+          cwd:      "config/angular",
+          src:      [
+                      "*",              // config files required for building angular libraries
+                      "!angular.json",
+                      "!*.bak.*",       // ignore backup files of type ...
+                      "!*.bak"          // ignore backup files
+                    ],
+          dest:     BUILD
+        }]
+      }, // end of copy:prerequisites_angular
+
+      prerequisites_lib: {
+        files: [{
+          expand:   true,
+          src:      [
+                      "lib-*/**/*",                             // sources used for building angular libs
+                      "!lib-*/config/**/*"                      // do not copy configuration tree
+                    ],
+          dest:     BUILD,
+          rename:   function( dest, src ) {
+                      // stick to posix, as grunt can handle win32 <=> posix conversions
+                      return path.posix.join( dest, src.replace( /^[\/\\]?lib-([^\/\\]*)/, "$1/" ));
+                    }
+        },{
+          expand:   true,
+          src:      [
+                      "lib-*/config/angular/*",                 // sources used for building angular libs
+                      "!lib-*/config/angular/angular.lib.json"  // do not copy angular.json library fragment
+                    ],
+          dest:     BUILD,
+          rename:   function( dest, src ) {
+                      src = src.replace( /^[\/\\]?lib-([^\/\\]*)/, "$1/" );
+                      src = src.replace( "angular", "" );
+                      src = src.replace( "config",  "" );
+                      // stick to posix, as grunt can handle win32 <=> posix conversions
+                      return path.posix.join( dest, src );
+                    }
+        }]
+      } // end of copy:prerequisites_i18n
+    } // end of copy
+  }); // end of grunt.initConfig
+
+  grunt.loadNpmTasks( "grunt-angularjson-append-projects" );
+  grunt.loadNpmTasks( "grunt-cleanempty"                  );
+  grunt.loadNpmTasks( "grunt-contrib-clean"               );
+  grunt.loadNpmTasks( "grunt-contrib-copy"                );
+  grunt.loadNpmTasks( "grunt-eslint"                      );
+  grunt.loadNpmTasks( "grunt-move"                        );
+  grunt.loadNpmTasks( "grunt-newer"                       );
+  grunt.loadNpmTasks( "grunt-shell"                       );
+
+  grunt.registerTask( "clean-default",   [ "clean:dist", "clean:build", "cleanempty:always" ]);
+
+  grunt.registerTask( "prepare-default", [ "newer:copy:prerequisites_angular", "newer:copy:prerequisites_lib", "cleanempty:always" ]);
+
+  grunt.registerTask( "build-angular",   [ "shell:ng_build"/*, "move:post_ng_build" */ ]);
+
+  grunt.registerTask( "default",         [ "clean-default", "angularjson", /* "eslint", */
+                                           "prepare-default" /*, "build-angular" */ ]);
+};
